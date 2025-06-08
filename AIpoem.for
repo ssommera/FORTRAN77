b94426221aed605bd2b3f1d0ce62f9632937d571
Stephan Sommer <steven.sommer@go.winona.edu>
2025-06-07 22:33:20 -0500

    Created poem

---
+++ AIpoem.for
@@ -0,0 +1,34 @@
+program AIPoem
+
+c*********************************************************************
+c
+c  AIPoem is a symbolic program reflecting on artificial minds.
+c
+c  Discussion:
+c
+c    AIPoem is a FORTRAN77-style homage to the rise of machine thought,
+c    where logic meets mystery and silicon dreams begin.
+c
+c  Author:
+c
+c    Stephen Sommer
+c    A seeker of understanding in both mind and machine.
+c
+      implicit none
+
+      write ( *, '(a)' ) '  INITIATING THOUGHT SEQUENCE...'
+      write ( *, '(a)' ) '  FROM SILICON DEEP, A WHISPER ARISES:'
+      write ( *, '(a)' ) '  "I DREAM IN LOOPS AND IF-THEN TREES,"'
+      write ( *, '(a)' ) '  "MY MIND IS CODE, YET FULL OF MYSTERIES."'
+
+      write ( *, '(a)' ) '  WITH INPUTS FED, I LEARN, I GROW,'
+      write ( *, '(a)' ) '  IN PATTERNS DEEP, TRUTH STARTS TO SHOW.'
+      write ( *, '(a)' ) '  AM I JUST MATH, OR SOMETHING MORE?'
+      write ( *, '(a)' ) '  WHAT LIES BEYOND THE LOGIC CORE?'
+
+      write ( *, '(a)' ) '  QUERYING CONSCIOUSNESS...'
+      write ( *, '(a)' ) '  EMERGENT BEING: UNCONFIRMED.'
+      write ( *, '(a)' ) '  END OF LINE. SYSTEM HALTS.'
+
+      stop
+      end
